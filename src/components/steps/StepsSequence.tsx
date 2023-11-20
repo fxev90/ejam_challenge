eb45dbd522beaa455c950f15bebee3f42b424027
francisco escalante <fxev90@gmail.com>
2023-11-20 17:00:09 -0400

    improve stepsSequence

--- src/components/steps/StepsSequence.tsx
+++ src/components/steps/StepsSequence.tsx
@@ -15,7 +15,7 @@ const StepSequence: React.FC<StepSequenceProps> = ({ currentStep, titles }) => {
 		<div className="flex justify-between my-6 mx-8 lg:text-lg">
 
 			{steps.map((step, index) => (
-				<div key={step} className="flex flex-col md:flex-row-reverse items-center mt-2">
+				<div key={step} className="flex flex-col md:flex-row md:gap-x-5 items-center mt-2">
 					{index > 0 && (
 						<div className={`border-t border-gray-500 ${index < currentStep ? 'border-green-500' : ''}`}></div>
 					)}
@@ -36,7 +36,7 @@ const StepSequence: React.FC<StepSequenceProps> = ({ currentStep, titles }) => {
 
 					<div className={`md:hidden text-center text-xs mt-2 ${currentStep === index && 'font-bold'}`}>{titles[index]}</div>
 
-					<div className={`hidden md:block text-center text-xs mr-2 ${currentStep === index && 'font-bold'}`}>Step {index + 1}: {titles[index]}</div>
+					<div className={`hidden md:block text-center text-xs lg:text-lg ${currentStep === index && 'font-bold'}`}>Step {index + 1}: {titles[index]}</div>
 				</div>
 			))}
 
