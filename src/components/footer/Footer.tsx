129f976dd501436a6ceff112c9541c768d63407e
francisco escalante <fxev90@gmail.com>
2023-11-20 16:34:31 -0400

    fix footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,12 +1,12 @@
-export  function Footer({}) {
-	return (<div className="footer bg-[#252F3D] text-white text-xs py-4">
-	  <div className="flex justify-center my-2">
-		 <h1>Copyright (c) 2023</h1>
-		 <span className="h-4 w-[1px] bg-slate-200 mx-4"></span>
-		 <h1>Clarifionsupport@clarifion.com</h1>
-	  </div>
-	  <div>
-		 <h1 className="text-center">Secure 256-bit SSL encryption.</h1>
-	  </div>
+export function Footer() {
+	return (<div className="footer bg-[#252F3D] text-white text-xs py-4 lg:flex lg:justify-between lg:px-32">
+		<div className="flex justify-center my-2">
+			<h1>Copyright (c) 2023</h1>
+			<span className="h-4 w-[1px] bg-slate-200 mx-4"></span>
+			<h1>Clarifionsupport@clarifion.com</h1>
+		</div>
+		<div className="flex items-center justify-center">
+			<h1>Secure 256-bit SSL encryption.</h1>
+		</div>
 	</div>);
- }
+}
