3ce6bdf324353891b64d06ed07aa983c807ce1da
Francisco Escalante <fxev90@gmail.com>
2023-11-20 17:26:11 -0400

    Fixes implicit type in button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,4 @@
-export function Button({ text }) {
+export function Button({ text } :{ text: string }) {
 	return (<button className="w-full   h-14 border rounded-full text-white bg-[#59AE43]  flex items-center justify-center gap-10">
 		{text}
 	</button>);
